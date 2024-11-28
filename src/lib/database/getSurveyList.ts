35f9ca1d57d543cc8db2d5e81d2672a58eed8841
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-11-29 01:12:50 +0700

    refactor(database): update query
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/lib/database/getSurveyList.ts
+++ src/lib/database/getSurveyList.ts
@@ -2,6 +2,6 @@ import type { Survey } from '$types';
 import { pool } from './connection';
 
 export const getSurveyList = async (): Promise<Survey[]> => {
-  const { rows: survey } = await pool.query('SELECT name,description FROM t_survey;');
+  const { rows: survey } = await pool.query('SELECT id,name,description FROM t_survey;');
   return survey;
 };
