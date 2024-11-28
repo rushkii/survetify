aa4281fc5c6f4f23b236bd07732f1969d7b4e509
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-11-28 18:08:44 +0700

    feat(/): display UI survey list
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+  import type { PageServerData } from './$types';
+
+  let { data }: { data: PageServerData } = $props();
+</script>
+
 List of Survey
+
+{#each data.survey as s}
+  {s.name} {s.description}
+{/each}
