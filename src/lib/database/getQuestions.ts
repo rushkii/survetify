3e596dc761d8e5dd199d8ac968b1fd7c8b1fe6e0
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-12-01 23:11:28 +0700

    feat:fix(database|survey): add conditional statement & try/catch to handle query error
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/lib/database/getQuestions.ts
+++ src/lib/database/getQuestions.ts
@@ -1,30 +1,36 @@
 import type { Answer, Question, QuestionRaw } from '$types';
 import { pool } from './connection';
 
-export const getQuestions = async (surveyId: string): Promise<Question[]> => {
-  const { rows: questions }: { rows: QuestionRaw[] } = await pool.query(
-    `
-    SELECT
-        sq.question,
-        ARRAY_AGG(
-            '{ "id": ' || sa.id || ', "answer": "' || sa.answer || '" }'
-        ORDER BY sa.id) AS answers
-    FROM
-        t_survey_questions sq
-    LEFT JOIN
-        t_survey_answers sa ON sa.question_id = sq.id
-    WHERE
-        sq.survey_id = $1
-    GROUP BY
-        sq.id
-    ORDER BY
-        sq.id;
-  `,
-    [surveyId]
-  );
+export const getQuestions = async (surveyId: string): Promise<Question[] | null> => {
+  try {
+    const { rows: questions }: { rows: QuestionRaw[] } = await pool.query(
+      `
+      SELECT
+          sq.question,
+          ARRAY_AGG(
+              '{ "id": ' || sa.id || ', "answer": "' || sa.answer || '" }'
+          ORDER BY sa.id) AS answers
+      FROM
+          t_survey_questions sq
+      LEFT JOIN
+          t_survey_answers sa ON sa.question_id = sq.id
+      WHERE
+          sq.survey_id = $1
+      GROUP BY
+          sq.id
+      ORDER BY
+          sq.id;
+    `,
+      [surveyId]
+    );
 
-  return questions.map((e) => ({
-    ...e,
-    answers: e.answers.map<Answer>((e) => JSON.parse(e))
-  }));
+    if (!questions) return null;
+
+    return questions.map((e) => ({
+      ...e,
+      answers: e.answers.map<Answer>((e) => JSON.parse(e))
+    }));
+  } catch {
+    return null;
+  }
 };
--- src/routes/(protected)/survey/[id]/+page.server.ts
+++ src/routes/(protected)/survey/[id]/+page.server.ts
@@ -1,7 +1,11 @@
 import * as db from '$lib/database';
 import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
 export const load = (async ({ params }) => {
   const questions = await db.getQuestions(params.id);
+
+  if (!questions) redirect(302, '/');
+
   return { questions };
 }) satisfies PageServerLoad;
