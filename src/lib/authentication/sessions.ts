87d630ebbe7ef1486d4c9e426a02661feb2cab75
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-12-01 23:00:07 +0700

    chore: move COOKIE_OPTIONS to constants.ts
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/lib/authentication/sessions.ts
+++ src/lib/authentication/sessions.ts
@@ -1,15 +1,5 @@
-import { dev } from '$app/environment';
-import { SESSION_COOKIE_NAME } from '$lib/constants';
+import { COOKIE_OPTIONS, SESSION_COOKIE_NAME } from '$lib/constants';
 import type { Cookies } from '@sveltejs/kit';
-
-type CookieOptionType = import('cookie').CookieSerializeOptions & { path: string };
-
-const COOKIE_OPTIONS = {
-  path: '/',
-  httpOnly: true,
-  secure: !dev,
-  sameSite: 'lax'
-} satisfies CookieOptionType;
 
 export const get = (cookies: Cookies) => {
   const cookie = cookies.get(SESSION_COOKIE_NAME);
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,10 @@
+import { dev } from '$app/environment';
+
 export const SESSION_COOKIE_NAME = 'survetify-session';
+
+export const COOKIE_OPTIONS = {
+  path: '/',
+  httpOnly: true,
+  secure: !dev,
+  sameSite: 'lax'
+} satisfies import('cookie').CookieSerializeOptions & { path: string };
