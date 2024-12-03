b4c6d67f0dac778e1601b0dae0e3fd3f461cfa8f
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-12-03 19:02:01 +0700

    refactor(utils): rename transform question function
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/lib/utils/transform.ts
+++ src/lib/utils/transform.ts
@@ -1,11 +1,13 @@
 import type { Question } from '$types';
 import { compile } from 'mdsvex';
 
-export const transformQuestion = (questions: Question[]) => {
-  return questions.map(async (q) => ({
-    question: (await compile(q.question))!.code,
-    answers: await Promise.all(
-      q.answers.map(async (a) => ({ id: a.id, answer: (await compile(a.answer))!.code }))
-    )
-  }));
+export const question = async (questions: Question[]): Promise<Question[]> => {
+  return await Promise.all(
+    questions.map(async (q) => ({
+      question: (await compile(q.question))!.code,
+      answers: await Promise.all(
+        q.answers.map(async (a) => ({ id: a.id, answer: (await compile(a.answer))!.code }))
+      )
+    }))
+  );
 };
