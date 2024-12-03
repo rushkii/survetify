1a7ad7891d3c29f1d1009c8d5b4481acd86ab167
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-12-03 18:56:59 +0700

    feat(deps): add svelte enchanced-img library
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
     sveltekit(),
+    enhancedImages(),
     paraglide({
       project: './project.inlang',
       outdir: './src/lib/paraglide'
