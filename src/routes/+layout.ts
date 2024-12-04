86134da846c927b51833b3f7a7aa01bacb58842e
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-12-04 18:45:05 +0700

    feat(main-layout-client): add toast trigger
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,9 @@
+import toast from 'svelte-5-french-toast';
 import type { LayoutLoad } from './$types';
 
 export const ssr = false;
 
 export const load: LayoutLoad = async ({ data }) => {
+  if (data.errorMessage) toast(data.errorMessage, { icon: 'ℹ️' });
   return { data };
 };
