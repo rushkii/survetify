041fe0cdd70c87f0bc8f7e017429fa7c2f9ef99c
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-12-04 19:51:53 +0700

    feat(routes): use newly created anchor component
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { element } from '$components';
   import type { PageData } from './$types';
 
   let { data }: { data: PageData } = $props();
@@ -11,13 +12,14 @@
     <ul class="w-full space-y-3 px-5">
       {#each data.survey as s}
         <li class="w-full">
-          <a
+          <element.Anchor
             href="/survey/{s.id}"
+            data-sveltekit-preload-data="tap"
             class="flex h-full w-full flex-col gap-y-3 rounded-lg bg-neutral-200 p-3 transition-colors duration-200 hover:bg-neutral-300"
           >
             <h3 class="text-3xl font-bold">{s.name}</h3>
             <div>{s.description}</div>
-          </a>
+          </element.Anchor>
         </li>
       {/each}
     </ul>
